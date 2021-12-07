0df0578c8bf273a77f432534b45e3e271bed1543
SgnvJaber <sgnvunleashed5@gmail.com>
2021-12-07 16:21:55 +0200

    Added reverse function for dictionary

--- python_for_testers/lesson02/UniqueLetters.py
+++ python_for_testers/lesson02/UniqueLetters.py
@@ -1,3 +1,4 @@
+# Ex1
 def unique_letters(word):
     unique_word = []
     for letter in word:
@@ -6,16 +7,24 @@ def unique_letters(word):
     return unique_word
 
 
+# Ex2
 def count_letter_occurrence(word):
     occurrence = {}
-    for letter in word:
+    temp = word.replace(" ", "").upper()
+    for letter in temp:
         if letter in occurrence.keys():
             count = occurrence.get(letter)
             occurrence[letter] = count + 1
         if letter not in occurrence.keys():
             occurrence[letter] = 1
-    return occurrence
+    # Source:https://www.geeksforgeeks.org/python-reverse-dictionary-keys-order/
+    dic_occurrence = dict(reversed(list(occurrence.items())))
+    return dic_occurrence
 
 
+print("Ex1:")
 print(unique_letters("hello"))
-print(count_letter_occurrence("Hello World"))
+print("Ex2:")
+my_dict = count_letter_occurrence("Hello World")
+for x in my_dict:
+    print(x + ":" + str(my_dict[x]))
